e6b0e0eb82c03d6e84217f4ebe6fd3fc2b4c1ff1
Hineill <hineill.cespedes@pucp.edu.pe>
2024-04-04 21:00:28 -0500

    FIRST COMMIT

---
+++ src/main/java/com/example/labgtics/LabgticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.labgtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabgticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabgticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/labgtics/LabgticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labgtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabgticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
